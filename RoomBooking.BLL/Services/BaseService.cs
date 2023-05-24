d57ba58783f9567bcd22f864f89d4c6492ec1321
nmquang21 <43206310+nmquang21@users.noreply.github.com>
2023-05-24 16:01:31 +0700

    Update BaseService.cs

--- RoomBooking.BLL/Services/BaseService.cs
+++ RoomBooking.BLL/Services/BaseService.cs
@@ -225,7 +225,7 @@ namespace RoomBooking.BLL.Services
                 // Gán giá trị cho các thuộc tính của đối tượng
                 _emailSettings.EmailId = "bookingroomad@gmail.com";
                 _emailSettings.Name = "Support - Pro Code Guide";
-                _emailSettings.Password = "jeopyuiouqargvgs";
+                _emailSettings.Password = "rupnjiqcjggygrol";
                 _emailSettings.Host = "smtp.gmail.com";
                 _emailSettings.Port = 465;
                 _emailSettings.UseSSL = true;
